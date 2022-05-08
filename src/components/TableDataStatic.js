cca6ee7a384720434f79f0eb4c9a908dd8fc0eee
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-08 14:06:58 -0500

    Se creo el componente para que renderizara una tabla de datos esaticos

---
+++ src/components/TableDataStatic.js
@@ -0,0 +1,28 @@
+import React from "react";
+import MUIDataTable from "mui-datatables";
+
+const columns = ["Name", "Company", "City", "State"];
+
+const data = [
+  ["Joe James", "Test Corp", "Yonkers", "NY"],
+  ["John Walsh", "Test Corp", "Hartford", "CT"],
+  ["Bob Herm", "Test Corp", "Tampa", "FL"],
+  ["James Houston", "Test Corp", "Dallas", "TX"],
+];
+
+const options = {
+  filterType: "checkbox",
+};
+
+export const TableDataStatic = () => {
+  return (
+    <div>
+      <MUIDataTable
+        title={"Employee List"}
+        data={data}
+        columns={columns}
+        options={options}
+      />
+    </div>
+  );
+};
