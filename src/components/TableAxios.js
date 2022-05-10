fff77be0f38729d161d6e6ad1040fe6e3466c879
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-09 23:10:49 -0500

    Funcionalidad para borarr los datos del data-table

--- src/components/TableAxios.js
+++ src/components/TableAxios.js
@@ -19,15 +19,90 @@ export const TableAxios = () => {
   };
 
   const colums = [
-    { name: "id", label: "ID" },
-    { name: "title", label: "TITLE" },
-    { name: "price", label: "PRICE" },
-    { name: "category", label: "CATEGORY" },
-    { name: "description", label: "DESCRIPTION" },
+    {
+      name: "id",
+      label: "ID",
+      options: {
+        filter: false,
+        sort: true,
+      },
+    },
+    { name: "title", label: "TITLE", options: { filter: false, sort: false } },
+    { name: "price", label: "PRICE", options: { filter: false, sort: false } },
+    {
+      name: "category",
+      label: "CATEGORY",
+      options: { filter: true, sort: false },
+    },
+    {
+      name: "description",
+      label: "DESCRIPTION",
+      options: { filter: false, sort: false },
+    },
+    {
+      name: "delete",
+      options: {
+        filter: false,
+        sort: false,
+        empty: true,
+
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return (
+            <button
+              onClick={() => {
+                console.log(value);
+                console.log(tableMeta.rowData[0]);
+                // console.log(updateValue);
+                const dataNew = products.filter(
+                  (p) => p.id !== tableMeta.rowData[0]
+                );
+                console.log(dataNew);
+                setProducts(dataNew);
+              }}
+            >
+              Delete
+            </button>
+          );
+        },
+      },
+    },
+    {
+      name: "edit",
+      options: {
+        filter: false,
+        sort: false,
+        empty: true,
+
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return (
+            <button
+              onClick={() => {
+                console.log(value);
+                console.log(tableMeta.rowData[0]);
+                window.alert(`Clicked "Edit" for row ${tableMeta.rowIndex}`);
+              }}
+            >
+              Edit
+            </button>
+          );
+        },
+      },
+    },
   ];
 
   const options = {
     filterType: "checkbox",
+    onRowsDelete: (rowsDeleted, data, dataIndex, e) => {
+      console.log(rowsDeleted);
+      console.log(data);
+      console.log(dataIndex);
+
+      const idsToDelete = rowsDeleted.data.map((d) => products[d.dataIndex].id); // array of all ids to to be deleted
+      console.log(idsToDelete);
+      //   const idsToDelete = rowsDeleted.data.map((d) => data[d.dataIndex].id); // array of all ids to to be deleted
+      //   console.log(idsToDelete);
+      //   http.delete(idsToDelete, res).then(window.alert("Deleted!")); // your delete request here
+    },
   };
 
   return (
